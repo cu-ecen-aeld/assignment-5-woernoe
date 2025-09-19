f76bd07f55dd02b443ba7c6ae72eaa3726bb0d03
woernoe <we@ernstware.de>
2025-09-19 15:26:52 +0200

    Assignment 5-2 V02

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -8,7 +8,8 @@
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
 #AESD_ASSIGNMENTS_VERSION = f2a4c310ee10714f842c272b164828db2a4df0f7
-AESD_ASSIGNMENTS_VERSION = 809b6179a3abbfcdaad646f142fe1cee8a73f0c0
+#AESD_ASSIGNMENTS_VERSION = 809b6179a3abbfcdaad646f142fe1cee8a73f0c0
+AESD_ASSIGNMENTS_VERSION = f038a50d3d5ce8baf8e247b859abdd4ab456bdab
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
